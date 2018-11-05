35a8065433b2e1cca466418c44030a4f73cd570c
Nik <38110784+jjjamer@users.noreply.github.com>
2018-11-05 14:22:02 -0600

    Added Test Scene: Added toggle mechanic (on key 'e' for now) and decorated room 3 a little bit
    Added new test scene and new room3 scene that has art assets in it.  Sciripted a rough mechanic function. Added stairs and a platform to room3.  Added some testing things into the test scene

---
+++ Steve the Sorcerer's Great Adventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //I Understand that this isn't really a player controller, more of a player manager, but i don't want to change the name
+
+    //We use tags to distinguish which objects can get toggled in this game
+
+    //Key for the mechanic
+    [Tooltip("Which key activates the toggle of the environment")]public KeyCode mechanicKey = KeyCode.E;
+    [Header("Materials that will get swaped")]
+    public Material blueOn;
+    public Material blueOff;
+    public Material redOn;
+    public Material redOff;
+    //Current layout is blue = false and red = true. If you want to change it, go to the mechanicFlip() function/method/whatever
+    private bool mechanicToggle;
+    private GameObject[] blueObjects;
+    private GameObject[] redObjects;
+
+    private void Start()
+    {
+        //Finding all objects that can be toggled
+        if(blueObjects == null)
+        {
+            blueObjects = GameObject.FindGameObjectsWithTag("Blue");
+        }
+        if(redObjects == null)
+        {
+            redObjects = GameObject.FindGameObjectsWithTag("Red");
+        }
+        //Need to run it once so everything gets a material and whatnot
+        mechanicFlip();
+    }
+
+    //Didn't allow fast pressing of key
+    /*private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(mechanicKey))
+        {
+            mechanicFlip();
+        }
+    }
+    */
+    private void Update()
+    {
+        if (Input.GetKeyDown(mechanicKey))
+        {
+            mechanicFlip();
+        }
+    }
+
+    private void mechanicFlip()
+    {
+        if (!mechanicToggle)
+        {
+            //Swap blue with red
+            foreach(GameObject blue in blueObjects)
+            {
+                blue.gameObject.GetComponent<MeshCollider>().enabled = false;
+                blue.gameObject.GetComponent<MeshRenderer>().material = blueOff;
+            }
+            //now for red to turn on
+            foreach(GameObject red in redObjects)
+            {
+                red.gameObject.GetComponent<MeshCollider>().enabled = true;
+                red.gameObject.GetComponent<MeshRenderer>().material = redOn;
+            }
+        }
+        else if (mechanicToggle)
+        {
+            //turn blue on
+            foreach (GameObject blue in blueObjects)
+            {
+                blue.gameObject.GetComponent<MeshCollider>().enabled = true;
+                blue.gameObject.GetComponent<MeshRenderer>().material = blueOn;
+            }
+            //now for red to turn off
+            foreach (GameObject red in redObjects)
+            {
+                red.gameObject.GetComponent<MeshCollider>().enabled = false;
+                red.gameObject.GetComponent<MeshRenderer>().material = redOff;
+            }
+        }
+        mechanicToggle = !mechanicToggle;
+    }
+}
