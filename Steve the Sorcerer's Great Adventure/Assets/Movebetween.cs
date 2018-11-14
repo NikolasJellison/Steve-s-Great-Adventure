d7627557b541a8425e8c96ecb98f91c918f3e8dc
Nikolas <38110784+jjjamer@users.noreply.github.com>
2018-11-13 18:56:51 -0600

    Quick move-between script (maybe works, maybe not)

---
+++ Steve the Sorcerer's Great Adventure/Assets/Movebetween.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movebetween : MonoBehaviour {
+
+    public Transform[] waypoints;
+    public float speed;
+    private float step;
+    private int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+        step = speed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[counter].position, step);
+        if(Vector3.Distance(transform.position, waypoints[counter].position) < .1)
+        {
+            if(counter <= waypoints.Length)
+            {
+                counter++;
+            }
+            else
+            {
+                counter = 0;
+            }
+        }
+	}
+}
